b4adeaa96ad26a8a10fb05743dfc3943bed43edf
Kurt Schneider <k.w.schneid@gmail.com>
2017-03-23 14:42:10 -0400

    First Push

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,23 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// contact scroll fixed position
+// get initial position of the element
+let scroll = $('.contact').offset().top;   
 
-class App extends React.Component {
-	constructor() {
-		super();
-	}
-	componentWillMount(){
-		console.log('Mounting');
-	}
-	render(){
-		return (
-			<div>
-				<h1>Hello!</h1>
-			</div>
-		)
-	}
-	componentDidMount(){
-		console.log('Mounted');
-	}
-}
+// window.dispatchEvent(new Event('resize'));
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// assign scroll event listener
+$(window).scroll(function() {             
+
+	// get current position
+	let currentScroll = $(window).scrollTop() + 25; 
+	let bottomScroll = $(window).scrollTop() + $(window).height() - 275;
+
+	if (currentScroll >= scroll) { 
+	// apply position: fixed if you
+		$('.contact').css({                 // scroll to that element or below it
+			position: 'fixed',
+			// top: '50%',
+			// left: '0',
+			// transform: 'translate(0, -50%)',
+			// margin: '0 25px'
+		});
+		$('.main').css({
+			margin: '0 0 0 350px',
+			width: '100%'
+		});
+   } else if (bottomScroll >= scroll) {                               // apply position: static
+		$('.contact').css({               
+		// if you scroll above it
+		   position: 'static',
+		   // top:'0',
+			margin: '25px',
+			// transform: 'none'
+		});
+		$('.main').css({
+			width: '100%',
+			margin:'0'
+		});
+   }
+});
+
+//275 px bottom
+
+// Smooth out animation on any link starting with #
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+        let target = this.hash,
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 40
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+}); 
