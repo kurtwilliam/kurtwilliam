0a34d4049f4a968afb7e202c9d3f77ee290676ae
Kurt Schneider <k.w.schneid@gmail.com>
2017-03-26 18:10:46 -0400

    Almost completed - JS, responsive, Contact Form

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,56 +1,112 @@
-// contact scroll fixed position
-// get initial position of the element
-let scroll = $('.contact').offset().top;   
+// JavaScript Media Query for Contact scroll bar
 
-// window.dispatchEvent(new Event('resize'));
+$(window).resize(function(){
+	scrollFix();
+})
 
-// assign scroll event listener
-$(window).scroll(function() {             
+function scrollFix(){
+	// get initial position of the element
+	let scroll = $('.contact').offset().top;
 
-	// get current position
-	let currentScroll = $(window).scrollTop() + 25; 
-	let bottomScroll = $(window).scrollTop() + $(window).height() - 275;
+	// get contact form height to offset bottom position
+	let contactH = $('.contact').height();
 
-	if (currentScroll >= scroll) { 
-	// apply position: fixed if you
-		$('.contact').css({                 // scroll to that element or below it
-			position: 'fixed',
-			// top: '50%',
-			// left: '0',
-			// transform: 'translate(0, -50%)',
-			// margin: '0 25px'
+	// assign scroll event listener
+	$(window).scroll(function() {
+		// get current position, bottom position, and doc height
+		let currentScroll = $(window).scrollTop() + 25; 
+		let bottomScroll = $(window).scrollTop() + $(window).height();
+		let docHeight = $(document).height();
+		let footerHeight = $('footer').height();
+		let contactHeight = $('.contact').height()+105;
+		let bodyW = $('body').width();
+		console.log('bodW', bodyW )
+
+		if (bodyW >= 978 ) {
+
+			// if scroll height is below header and above footer:
+			if (currentScroll >= scroll && currentScroll <= (docHeight - (footerHeight + contactHeight)))  { 
+			// apply position: fixed if you
+				$('.contact').css({                 
+				// scroll to that element or below it
+					position: 'fixed',
+					bottom: '0',
+					width:'300px'
+				});
+				$('.wrapper').css({                 
+					margin: '25px 0 0 375px'
+				});
+
+			// else if the bottom of contact is at the footer:
+			} else if (bottomScroll >= docHeight - footerHeight ) {
+				$('.contact').css({               
+					// if you scroll above it
+					position: 'absolute',
+					bottom: `${footerHeight}px`,
+					top: 'auto',
+					left: '0',
+					margin: '25px',
+					height:'calc( 100vh - 50px )'
+				});
+				
+			// else if scroll position is within header:
+		   } else if (scroll >= currentScroll) {
+		   // apply position: static
+				$('.contact').css({               
+				   position: 'relative',
+					margin: '25px',
+					bottom: '0'
+				});
+				$('.wrapper').css({                 
+					margin: '25px'
+				});
+
+		   // else if the bottom of the document is above the header (to reset fixed pos):
+		   } else if ( bottomScroll <= docHeight - footerHeight) {
+		   	// apply position: fixed
+				$('.contact').css({                 
+					position: 'fixed',
+					top: '0',
+					bottom: '0'
+				});
+				$('.wrapper').css({                 
+					margin: '25px 0 0 375px',
+					width: '100%'
+				});
+		   }
+		} else{
+			$('.contact').css({                 
+				background: '$main2',
+				position: 'relative',
+				width: 'calc(100% - 50px)',
+				margin: '25px',
+				height: 'auto',
+				bottom:'0'
+			});
+			$('.wrapper').css({                 
+				margin: '25px',
+				width: '100%'
+			});
+		}
+	});
+}
+
+function linkScroll(){
+	$('a[href^="#"]').on('click',function (e) {
+		e.preventDefault();
+		let target = this.hash,
+		$target = $(target);
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top 
+		}, 900, 'swing', function () {
+			window.location.hash = target;
 		});
-		$('.main').css({
-			margin: '0 0 0 350px',
-			width: '100%'
-		});
-   } else if (bottomScroll >= scroll) {                               // apply position: static
-		$('.contact').css({               
-		// if you scroll above it
-		   position: 'static',
-		   // top:'0',
-			margin: '25px',
-			// transform: 'none'
-		});
-		$('.main').css({
-			width: '100%',
-			margin:'0'
-		});
-   }
-});
+	});
+}
 
-//275 px bottom
 
 // Smooth out animation on any link starting with #
 $(document).ready(function(){
-    $('a[href^="#"]').on('click',function (e) {
-        e.preventDefault();
-        let target = this.hash,
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top - 40
-        }, 900, 'swing', function () {
-            window.location.hash = target;
-        });
-    });
+    linkScroll();
+    scrollFix();
 }); 
